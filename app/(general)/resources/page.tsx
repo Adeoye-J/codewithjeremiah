4a3b380e06ae96e00f827c719c548cd991becc2c
Jeremiah Bankole <bankolejeremiahadeoye@gmail.com>
2026-03-04 15:36:55 +0100

    Commenced the resources page

--- app/(general)/resources/page.tsx
+++ app/(general)/resources/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Hero from '@/components/general/Hero'
 
 const ResourcesPage = () => {
     return (
-        <div>ResourcesPage</div>
+        <div className='min-h-screen'>
+            <Hero 
+                title='Resources' 
+                subtitle='Find valuable resources for your tech journey'
+                action={[{"title": "Courses", "location": "/courses"}, {"title": "My Blogs", "location": "/blogs"}]} 
+                bgImage='hero-bg.jpg' 
+            />
+        </div>
     )
 }
 
